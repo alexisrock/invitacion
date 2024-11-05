4750559da9eafc927711c3fdafe756bfdcff8bb1
alexis bueno <alexisrock20gmail.com>
2024-11-05 09:59:39 -0500

    feature: se agrega la seleccion del carrucel

--- main.server.mjs
+++ main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZTZFJG3S.mjs";import"./chunk-CZXIDKIU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WMXB5PEP.mjs";import"./chunk-RD5MDMKD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
