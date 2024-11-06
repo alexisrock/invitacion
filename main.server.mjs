a0dc0d678dd6f7ecfc57d4b32b945e4cb5b64170
alexis bueno <alexisrock20gmail.com>
2024-11-06 09:03:23 -0500

    feature: se realiza el diseno responsive para el carrucel y los botones

--- main.server.mjs
+++ main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WMXB5PEP.mjs";import"./chunk-RD5MDMKD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TIF33UY5.mjs";import"./chunk-RD5MDMKD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
