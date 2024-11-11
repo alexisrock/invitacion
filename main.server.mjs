e1ecb4ac74f4826a278473adf7c73e54a7586685
alexis bueno <alexisrock20gmail.com>
2024-11-11 14:11:23 -0500

    feature:se termina lo que seria la primera version de la maqueta

--- main.server.mjs
+++ main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WTYBMTQX.mjs";import"./chunk-RD5MDMKD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SV7GVBJ6.mjs";import"./chunk-RD5MDMKD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
